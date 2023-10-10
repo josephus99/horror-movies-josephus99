7849a2ea67d47a041027a4b9395ba5db46bfc564
Joey Boggan <joeywillis@msn.com>
2023-10-09 23:15:32 -0400

    Horror movies

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,6 @@
--- Add your SQL here
+SELECT id AS "Movie_ID", name AS "Movie_Title", imdb_rating AS "Rating", genre, year
+FROM movies
+WHERE genre  = "horror"
+AND year  <= 1985
+ORDER BY Rating DESC 
+LIMIT 3;
